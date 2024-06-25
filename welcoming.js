24b70a95588b3a4f3f750f53758e8e48480a28fb
devaber <abirasery@gmail.com>
2024-06-26 02:58:59 +0300

    Add files via upload

---
+++ welcoming.js
@@ -0,0 +1,5 @@
+function mygreating() {
+    let thename=document.querySelector("#thename");
+    let thegreating=document.querySelector("#thegreating");
+    thegreating.innerHTML="So Nice To Have You, "+thename.value+" Let's Explore Our Solay System!";
+}
